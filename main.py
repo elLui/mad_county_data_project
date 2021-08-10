10343c9861622e996ef14ad569b2ad371d018cf5
elluis <elluis.codes@gmail.com>
2021-08-10 14:47:46 -0500

    initial commit // using GitHub for our project :: https://github.com/elLui
    added initial TODO list

---
+++ main.py
@@ -0,0 +1,21 @@
+"""
+    Project goals - scrape public data regarding Madison County Detention Center for visualization.
+
+initial todo will increase according to project demands and learning structure
+
+ TODO :: set-up web scraping and data collection for http://smartweb.madisoncountysheriffal.org/SmartWEBClient/Jail.aspx
+
+ TODO :: set-up database structure to store information
+
+ @author elluis.codes@gmail.com
+ @version 08102021
+
+"""
+
+if __name__ == '__main__':
+    """
+    using main for initial set-up and debugging
+
+    TODO :: once project gets off the ground begin setting up unittest
+    """
+

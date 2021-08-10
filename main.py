4e698494da03fed3e16dc1f547aea04943936bf7
elluis <elluis.codes@gmail.com>
2021-08-10 15:14:17 -0500

    added page_retrieval(url)     takes in a url as an argument and generates a request using 'import requests' library
    added save_page_as_text_binary(res)
        takes response object and writes data to a statically named text file that will overwrite per run
        data is written in 'wb' write binary at 100_000 bytes per cycle

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
     Project goals - scrape public data regarding Madison County Detention Center for visualization.
 
-initial todo will increase according to project demands and learning structure
+ initial todo will increase according to project demands and learning structure
 
  TODO :: set-up web scraping and data collection for http://smartweb.madisoncountysheriffal.org/SmartWEBClient/Jail.aspx
 
@@ -10,7 +10,49 @@ initial todo will increase according to project demands and learning structure
  @author elluis.codes@gmail.com
  @version 08102021
 
+ python3.9
+
 """
+import requests
+
+
+def page_retrieval(url):
+    # send a request to download the page from url
+    res = requests.get(url)
+
+    try:
+        # verify that page download occurred before continuation
+        res.raise_for_status()
+    except Exception as exc:
+        print('There was a problem : %s' % exc)
+
+        # test functions
+        # print(type(res))
+        #
+        # print(res.status_code)
+        #
+        # print(len(res.text))
+        #
+        # print(res.text[200:])
+        #
+        # print(res.text[:200])
+
+
+def save_page_as_text_binary(res):
+    # create or overwrite a file for data storage
+    # TODO :: future add option to name or dynamically name file for storage
+    response_file = open('mad_county_data.txt', 'wb')
+
+    for chunk in res.iter_content(100_000):
+        # iterate and write content at 100_000 bytes per cycle
+        response_file.write(chunk)
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     """
@@ -19,3 +61,4 @@ if __name__ == '__main__':
     TODO :: once project gets off the ground begin setting up unittest
     """
 
+    basic_page_retrieval("http://smartweb.madisoncountysheriffal.org/SmartWEBClient/Jail.aspx")
